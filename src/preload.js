ef18688292c5e862be804028589a8a545c319f6c
耿名伟 <huan02@foxmail.com>
2026-06-25 23:57:00 +0800

    feat: preload 暴露搜索 API 并初始化搜索入口

--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,24 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const { initSearch } = require('./search/index');
 
-// 暴露安全的搜索 API 给渲染进程
+/**
+ * 包装 IPC 监听，去除事件对象，只把数据传给回调
+ */
+function on(channel, callback) {
+  const wrapped = (event, ...args) => callback(...args);
+  ipcRenderer.on(channel, wrapped);
+}
+
 contextBridge.exposeInMainWorld('electronSearch', {
-  find: (text, options) => ipcRenderer.invoke('search:find', text, options),
+  find: (text, options) => ipcRenderer.invoke('search:find', { text, options }),
   stop: () => ipcRenderer.send('search:stop'),
   navigate: (direction) => ipcRenderer.send('search:navigate', direction),
-  onFound: (callback) => ipcRenderer.on('search:found', callback),
-  onToggle: (callback) => ipcRenderer.on('search:toggle', callback),
+  onFound: (callback) => on('search:found', callback),
+  onToggle: (callback) => on('search:toggle', callback),
 });
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', initSearch);
+} else {
+  initSearch();
+}
